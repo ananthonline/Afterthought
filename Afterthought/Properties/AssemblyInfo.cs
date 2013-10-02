76081e5113ed4f58d54d8fdd21b4e547a2aecfb6
Ananth Balasubramaniam <ananth@ananthonline.net>
2013-10-02 17:21:21 -0400

    Added Nuget package generation

--- Afterthought/Properties/AssemblyInfo.cs
+++ Afterthought/Properties/AssemblyInfo.cs
@@ -8,11 +8,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Afterthought")]
 [assembly: AssemblyDescription("A post-compilation type amender for .NET")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("VC3")]
-[assembly: AssemblyProduct("Afterthought")]
-[assembly: AssemblyCopyright("Copyright © VC3 2011")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -21,16 +16,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("7a29bbfd-b1a5-4366-bfe4-e92ba9b006c2")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.6")]
-[assembly: AssemblyFileVersion("1.0.6")]
